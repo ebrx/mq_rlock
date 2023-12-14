c1c9a7939e0ec754ecbf84764e7b18fdce4586a3
ebrx <ebrx@vip.qq.com>
2023-12-15 07:30:19 +0800

    # 更新提交

--- src/main/java/com/example/mqrlock/common/Consumer.java
+++ src/main/java/com/example/mqrlock/common/Consumer.java
@@ -15,34 +15,25 @@ import java.util.concurrent.CountDownLatch;
 public class Consumer {
     private final CountDownLatch latch = new CountDownLatch(1);
 
-    @RabbitListener(id = Contants.QUEUE,queues =Contants.QUEUE)
-    public void receive(String s, Message message, Channel channel){
+    @RabbitListener(id = Contants.QUEUE, queues = Contants.QUEUE)
+    public void receive(String s, Message message, Channel channel) {
         try {
             long startTime = System.currentTimeMillis();
-            log.info("delimiter :{},{}", s, startTime);
-//            // 模拟异步处理
-//            CompletableFuture.runAsync(() -> {
-//                try {
-//                    Thread.sleep(3000); // 模拟耗时操作
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                } finally {
-//                    latch.countDown(); // 处理完成后释放 latch
-//                }
-//            });
-//
-//
-//            log.info("等待异步处理完成...");
-//            latch.await(); // 等待异步处理完成
-//            log.info("确认完成, 耗时: {}", System.currentTimeMillis() - startTime);
+            log.info("delimiter: {}, {}", s, startTime);
 
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+            // 判断消息是否是被重新放回来的
+            if (message.getMessageProperties().isRedelivered()) {
+                log.info("消息是被重新放回来的");
+            } else {
+                log.info("消息是第一次投递");
+            }
 
-            log.info("{}确认完成,耗时:{}",s,System.currentTimeMillis() - startTime);
-        }catch (Exception e) {
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+            log.info("{}确认完成, 耗时: {}", s, System.currentTimeMillis() - startTime);
+        } catch (Exception e) {
             try {
                 log.info("发生异常");
-                channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,false);
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
